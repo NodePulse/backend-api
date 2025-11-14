727c75d377c9e63fb0e7ff5e9be31f9701791c0f
NodePulse <bharbeysachin@gmail.com>
2025-11-14 12:23:28 +0530

    isssues fixe sin api-gateway

--- services/api-gateway/src/routes/swaggerDocsRoutes.ts
+++ services/api-gateway/src/routes/swaggerDocsRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import swaggerUI from "swagger-ui-express";
-import { swaggerSpec } from "../config/authDocs";
+import { swaggerSpec } from "../config/authDocs.js";
 
 const swaggerDocsRouter = express.Router();
 
